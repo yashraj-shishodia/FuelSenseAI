a0466ea4ea6504c730f7528f0e5746487665982a
YASHRAJ SHISHODIA <yashrajshishodia@YASHRAJs-MacBook-Air.local>
2026-07-05 17:01:11 +0530

    Integrated Leaflet map with user location

--- static/js/location.js
+++ static/js/location.js
@@ -1,14 +1,10 @@
+let map;
+
 function getLocation() {
 
     if (navigator.geolocation) {
 
-        navigator.geolocation.getCurrentPosition(
-
-            success,
-
-            error
-
-        );
+        navigator.geolocation.getCurrentPosition(success, error);
 
     }
 
@@ -28,42 +24,71 @@ function success(position) {
 
     document.getElementById("result").innerHTML =
 
-        `
-        Latitude : ${latitude}<br>
-        Longitude: ${longitude}
-        `;
+    `
 
-    fetch("/location", {
+    Latitude : ${latitude}<br>
 
-        method: "POST",
+    Longitude : ${longitude}
 
-        headers: {
+    `;
 
-            "Content-Type": "application/json"
+    fetch("/location",{
+
+        method:"POST",
+
+        headers:{
+
+            "Content-Type":"application/json"
 
         },
 
-        body: JSON.stringify({
+        body:JSON.stringify({
 
-            latitude: latitude,
+            latitude:latitude,
 
-            longitude: longitude
+            longitude:longitude
 
         })
 
-    })
-
-    .then(response => response.json())
-
-    .then(data => {
-
-        console.log(data);
-
     });
+
+    createMap(latitude,longitude);
 
 }
 
-function error() {
+function createMap(latitude,longitude){
+
+    if(map){
+
+        map.remove();
+
+    }
+
+    map=L.map('map').setView([latitude,longitude],15);
+
+    L.tileLayer(
+
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+
+        {
+
+            attribution:'© OpenStreetMap contributors'
+
+        }
+
+    ).addTo(map);
+
+    L.marker([latitude,longitude])
+
+        .addTo(map)
+
+        .bindPopup("📍 You are here")
+
+        .openPopup();
+
+}
+
+function error(){
 
     alert("Location permission denied.");
 
