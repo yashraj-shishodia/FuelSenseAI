c25152ba128ccf619e90955992f48ccf8200302f
YASHRAJ SHISHODIA <yashrajshishodia@YASHRAJs-MacBook-Air.local>
2026-07-15 23:31:21 +0530

    push these changes

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 from database.db import initialize_database
 
+from database.feedback import initialize_feedback_database
+
+initialize_feedback_database()
+
 app = Flask(__name__)
 
 initialize_database()
