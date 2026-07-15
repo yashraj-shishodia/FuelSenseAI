867ff559031e339663563445ab47b59be3269ccf
YASHRAJ SHISHODIA <yashrajshishodia@YASHRAJs-MacBook-Air.local>
2026-07-16 00:04:34 +0530

    push these changes

--- ml/train_model.py
+++ ml/train_model.py
@@ -65,8 +65,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 # ----------------------------
 
 model = RandomForestRegressor(
-    n_estimators=300,
-    max_depth=15,
+    n_estimators=100,
+    max_depth=10,
     random_state=42,
     n_jobs=-1
 )
